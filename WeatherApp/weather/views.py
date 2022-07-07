5445802a4a73c2ed27c3728729a36459136d9b6d
Andrei Kardovich <akardovich@gmail.com>
2022-07-07 22:11:45 +0300

    geting weather from website

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
-    return render(request, 'weather/index.html')
+    appid = '0e3f0f3d420b4960e2d0eb6c637db1e7'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+
+    city = 'Minsk'
+    res = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"]
+    }
+
+    context = {'info': city_info}
+
+    return render(request, 'weather/index.html', context)
