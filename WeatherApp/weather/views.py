b050c9a6177145a66e6fa0c37ec6bb40d2b13de4
Andrei Kardovich <akardovich@gmail.com>
2022-07-09 08:54:35 +0300

    geting and outputing dates

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 import requests
+from .models import City
 
 def index(request):
     appid = '0e3f0f3d420b4960e2d0eb6c637db1e7'
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
 
-    city = 'Minsk'
-    res = requests.get(url.format(city)).json()
+    cities = City.objects.all()
 
-    city_info = {
-        'city': city,
-        'temp': res["main"]["temp"],
-        'icon': res["weather"][0]["icon"]
-    }
+    all_cities = []
 
-    context = {'info': city_info}
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]["icon"],
+            'wind': res["wind"]["speed"]
+        }
+        all_cities.append(city_info)
+
+    context = {'info': all_cities}
 
     return render(request, 'weather/index.html', context)
